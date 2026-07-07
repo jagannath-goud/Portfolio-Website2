a083330220124e5c9d6601cfe5e77308de50bd2d
Jagannath Goud <jagannathgoud54@gmail.com>
2026-07-08 00:39:43 +0530

    Add line break to STARTUP BUILDER text on landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -21,7 +21,7 @@ const Landing = ({ children }: PropsWithChildren) => {
               <div className="landing-h2-2" style={{ opacity: 0 }}>DEVELOPER</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">STARTUP BUILDER</div>
+              <div className="landing-h2-info">STARTUP<br />BUILDER</div>
               <div className="landing-h2-info-1" style={{ opacity: 0 }}>ENTREPRENEUR</div>
             </h2>
           </div>
